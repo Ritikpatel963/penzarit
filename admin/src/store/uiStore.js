7caaf2d8ca21c67d2833f4be40b4e54e479f9d45
Ritikpatel963 <rp09444467@gmail.com>
2026-03-29 22:56:55 +0530

    ritik pushing the admin panel code also

--- admin/src/store/uiStore.js
+++
@@ -1,7 +0,0 @@
-import { create } from "zustand";
-
-export const useUiStore = create((set) => ({
-  sidebarCollapsed: false,
-  toggleSidebar: () =>
-    set((s) => ({ sidebarCollapsed: !s.sidebarCollapsed })),
-}));
